ae942789b3575fe5eb713177b0bf3a809421f276
Thomas Citharel <tcit@tcit.fr>
2019-01-27 12:32:29 +0100

    Fix and add tests
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- apps/dav/js/settings-admin-carddav.js
+++ apps/dav/js/settings-admin-carddav.js
@@ -19,7 +19,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-"use strict";
+'use strict';
 
 $('#carddavSyncSystemAddressbook').change(function () {
 	var val = $(this)[0].checked;
